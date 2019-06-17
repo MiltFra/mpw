bed7efa829fcfe5fb0b0bc407e512f4f5d6154cc
Franz Miltz <public@miltz.me>
2019-06-17 15:45:53 +0200

    Moved analysis to separate file

---
+++ internal/chain/analyze.go
@@ -0,0 +1,70 @@
+package chain
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+
+	"github.com/miltfra/markov/internal"
+	"github.com/miltfra/markov/internal/out"
+)
+
+// Analyze reads a file and returns a chain which contains
+// probabilities corresponding to n characters followed by
+// another one.
+func Analyze(path string, n int, bufS int) (c *Chain) {
+	c = New(n, path+"-mc")
+	file, err := os.Open(path)
+	if err != nil {
+		out.Error("Could not open input. (" + path + ")")
+		panic(err)
+	}
+	defer file.Close()
+	buf := make([]byte, bufS)
+	state := 0
+	read := 0
+	c.writeMeta()
+	f, err := os.Stat(path)
+	go update(&read, float64(f.Size()))
+	var dct map[int][]int
+	for {
+		dct = make(map[int][]int)
+		count, err := file.Read(buf)
+		if count > 0 {
+			read += count
+			//out.Status(fmt.Sprintf("Read %v bytes; processing...", count))
+			actBuf := buf[:count]
+			for _, s := range actBuf {
+				s -= 31
+				if s > 95 {
+					s = 0
+				}
+				if dct[state] == nil {
+					dct[state] = make([]int, 96)
+				}
+				dct[state][s]++
+				state = internal.ExtendState(n, state, int(s))
+			}
+			c.Insert(dct)
+		}
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			out.Error("Could not read input. (" + path + ")")
+			break
+		}
+	}
+	read = -1
+	out.Status("Reading file complete.")
+	// TODO: Write meta to disk
+	return
+}
+
+func update(b *int, size float64) {
+	for *b != -1 {
+		fmt.Printf("Read %v bytes (%v%v)        \r", *b, float64(*b)*100/size, "%")
+		time.Sleep(1 * time.Second)
+	}
+}
--- internal/chain/chain.go
+++ internal/chain/chain.go
@@ -2,13 +2,9 @@ package chain
 
 import (
 	"encoding/gob"
-	"fmt"
-	"io"
 	"os"
 	"strings"
-	"time"
 
-	"github.com/miltfra/markov/internal"
 	"github.com/miltfra/markov/internal/out"
 	"github.com/miltfra/markov/internal/tree"
 )
@@ -38,58 +34,6 @@ func (c *Chain) Insert(v map[int][]int) {
 // directory d with a depth n.
 func New(n int, d string) *Chain {
 	return &Chain{n, d, tree.New(n, d), false}
-}
-
-// Analyze reads a file and returns a chain which contains
-// probabilities corresponding to n characters followed by
-// another one.
-func Analyze(path string, n int, bufS int) (c *Chain) {
-	c = New(n, path+"-mc")
-	file, err := os.Open(path)
-	if err != nil {
-		out.Error("Could not open input. (" + path + ")")
-		panic(err)
-	}
-	defer file.Close()
-	buf := make([]byte, bufS)
-	state := 0
-	read := 0
-	c.writeMeta()
-	f, err := os.Stat(path)
-	go update(&read, float64(f.Size()))
-	var dct map[int][]int
-	for {
-		dct = make(map[int][]int)
-		count, err := file.Read(buf)
-		if count > 0 {
-			read += count
-			//out.Status(fmt.Sprintf("Read %v bytes; processing...", count))
-			actBuf := buf[:count]
-			for _, s := range actBuf {
-				s -= 31
-				if s > 95 {
-					s = 0
-				}
-				if dct[state] == nil {
-					dct[state] = make([]int, 96)
-				}
-				dct[state][s]++
-				state = internal.ExtendState(n, state, int(s))
-			}
-			c.Insert(dct)
-		}
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			out.Error("Could not read input. (" + path + ")")
-			break
-		}
-	}
-	read = -1
-	out.Status("Reading file complete.")
-	// TODO: Write meta to disk
-	return
 }
 
 type meta struct {
@@ -133,11 +77,4 @@ func readMeta(p string) *Chain {
 	split := strings.Split(p, "/")
 	d := strings.Join(split[:len(split)-1], "/")
 	return &Chain{m.N, d, tree.Load(m.N, d), false}
-}
-
-func update(b *int, size float64) {
-	for *b != -1 {
-		fmt.Printf("Read %v bytes (%v%v)        \r", *b, float64(*b)*100/size, "%")
-		time.Sleep(1 * time.Second)
-	}
 }
